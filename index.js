6261734fe6639748ad35c5f52e0ffc53876ac648
Wilfred <wilfredbayudan@gmail.com>
2021-08-14 10:32:49 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// Add DOMContentLoaded Event Listener
+document.addEventListener("DOMContentLoaded", ()=> {
+  console.log("DOM loaded, ready to manipulate document.")
+  const text = document.querySelector("#text");
+  text.textContent = "This is really cool!";
+})
+
+console.log("This message is logged before the DOM is loaded.");
